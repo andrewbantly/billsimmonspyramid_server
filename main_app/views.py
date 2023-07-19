80ae69ddbaa27aa0401d38bc3a3d46047e58dd34
andrewbantly <andrewbantly@gmail.com>
2023-07-19 13:20:12 -0700

    update api endpoints

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-# from .import_data import read_json_files
 import json
 
 def index(request):
     return HttpResponse('<h1>Hello Andrew</h1>')
 
 def data(request):
-    print("request:")
-    print(request)
-    print("  ")
     with open('main_app/data/index.json', 'r') as file:
         req_data = json.load(file)
     return JsonResponse(req_data)
