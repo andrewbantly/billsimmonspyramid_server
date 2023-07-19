baf5bfa4397bcd1e4ce47761c4312276d31ee317
andrewbantly <andrewbantly@gmail.com>
2023-07-19 12:59:24 -0700

    data connection to json

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home/', views.data, name='home'),
     path('', views.index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from .import_data import read_json_files
+import json
 
 def index(request):
     return HttpResponse('<h1>Hello Andrew</h1>')
 
+def data(request):
+    # Call the function to read JSON files
+    # json_data = read_json_files()
+    with open('main_app/data/index.json', 'r') as file:
+        req_data = json.load(file)
+    # return render(req_data)
+    return json(req_data)
+    # return render(json_data)
